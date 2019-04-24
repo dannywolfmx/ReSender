48115f4887ce0685a8874c21476c48d39278e467
Daniel <dannywolfmx@gmail.com>
2019-04-24 18:21:29 -0500

    Se arregla la importacion de la clase

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 // Modules to control application life and create native browser window
-const {
-  app,
-  BrowserWindow,
-  ipcMain,
-  shell
-} = require('electron');
+const {app, BrowserWindow, ipcMain, shell} = require('electron');
 //Trabajar con rutas entre plataformas
 const path = require('path');
 
 const fs = require('fs');
-const directorioOC = path.join('.',"OrdenesDeCompra")
-const juice = require('juice');
-const dot = require('dot')
+const directorioOC = path.join('.', 'OrdenesDeCompra');
 const mkdirp = require('mkdirp');
 
-const ClienteEmail = require('./servidor/servidorEmail.js')
-
+const ClienteCorreo = require('./servidor/servidorEmail');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-let clienteEmail = new ClienteEmail();
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
@@ -37,90 +27,84 @@ function createWindow() {
     'node-integration': false,
     webPreferences: {
       nodeIntegrationInWorker: true,
-      devTools: true
-    }
-  })
+      devTools: true,
+    },
+  });
 
   // mainWindow.setMenu(null)
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/polymer/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/polymer/index.html`);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show(); // Thumbbar is not showing
-
   });
-    
-    //Crear el directorio para ordenes de compra si no existe 
+
+  //Crear el directorio para ordenes de compra si no existe
   if (!fs.existsSync(directorioOC)) {
     fs.mkdirSync(directorioOC);
   }
 
-  ipcMain.on("copiarArchivo", function (event, archivo) {
+  ipcMain.on('copiarArchivo', function(event, archivo) {
     if (archivo) {
-        let directorio = path.join(directorioOC, archivo.oc);
+      let directorio = path.join(directorioOC, archivo.oc);
       if (!fs.existsSync(directorio)) {
         fs.mkdirSync(directorio);
       }
-        console.log(archivo.ruta)
-        let rutaArchivo = path.join(directorio,archivo.nombre);
+      console.log(archivo.ruta);
+      let rutaArchivo = path.join(directorio, archivo.nombre);
       fs.createReadStream(archivo.ruta).pipe(fs.createWriteStream(rutaArchivo));
     }
-  })
+  });
 
-    ipcMain.on("abreArchivo",function(event,ruta){
-    let rutaArchivo = path.join(app.getAppPath(),ruta)
-    shell.showItemInFolder(rutaArchivo)
-  })
-
+  ipcMain.on('abreArchivo', function(event, ruta) {
+    let rutaArchivo = path.join(app.getAppPath(), ruta);
+    shell.showItemInFolder(rutaArchivo);
+  });
 }
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
-
-
+});
 
 function getDirectories(ruta) {
-    return fs.readdirSync(ruta).filter(function (file) {
-        let rutaArchivo = path.join(ruta,file)
+  return fs.readdirSync(ruta).filter(function(file) {
+    let rutaArchivo = path.join(ruta, file);
     return fs.statSync(rutaArchivo).isDirectory();
   });
 }
 
-
-
-ipcMain.on("enviarCorreo", function (event, templateCorreo) {
-    let clienteEmail = new ClienteEmail();
-    clienteEmail.enviarCorreo(templateCorreo);
-})
+ipcMain.on('enviarCorreo', function(event, templateCorreo) {
+  console.log(typeof ClienteCorreo);
+  let clientecorreo = new ClienteCorreo();
+  clientecorreo.enviarCorreo(templateCorreo);
+});
--- servidor/servidorEmail.js
+++ servidor/servidorEmail.js
@@ -1,92 +1,94 @@
-const nodemailer = require("nodemailer")
+'use strict';
+
+const nodemailer = require('nodemailer');
 const juice = require('juice');
-const dot = require('dot')
-export default class EmailServer(){
+const dot = require('dot');
+const path = require('path');
+class ClienteCorreo {
+  constructor() {
+    console.log('Hola');
+  }
 
-let trasporter = {};
-
-
-
-
-dameTemplateHTML(nombreTemplate) {
+  dameTemplateHTML(nombreTemplate) {
     var html = new Promise((resolve, reject) => {
-        let rutaTemplate = path.join('.','emails',nombreTemplate,'html.hbs')
-    fs.readFile(rutaTemplate, 'utf-8', (err, datos) => {
-      err ? reject(err) : resolve(datos)
-    })
-  })
+      let rutaTemplate = path.join('.', 'emails', nombreTemplate, 'html.hbs');
+      fs.readFile(rutaTemplate, 'utf-8', (err, datos) => {
+        err ? reject(err) : resolve(datos);
+      });
+    });
 
     var subject = new Promise((resolve, reject) => {
-    let rutaTemplate = path.join('.','emails',nombreTemplate,'subject.hbs')
-    fs.readFile(rutaTemplate, 'utf-8', (err, datos) => {
-      err ? reject(err) : resolve(datos)
-    })
-  })
+      let rutaTemplate = path.join(
+        '.',
+        'emails',
+        nombreTemplate,
+        'subject.hbs',
+      );
+      fs.readFile(rutaTemplate, 'utf-8', (err, datos) => {
+        err ? reject(err) : resolve(datos);
+      });
+    });
 
-  return Promise.all([html, subject])
-}
+    return Promise.all([html, subject]);
+  }
 
-
-guardaPlantilla(html, ruta, nombreArchivo) {
-  if (html && ruta) {
-    mkdirp(ruta, function (err) {
-      if (err) {
-        console.log("Error en la ruta esta")
-      } else {
-          let path = path.join(ruta,nombreArchivo)
-        if(!fs.existsSync(path)){
-          fs.writeFile(path, html, function(err){
-            if(err){
-              return -1
-            }else{
-              console.log("Archivo guardado")
-            }
-          })
+  guardaPlantilla(html, ruta, nombreArchivo) {
+    if (html && ruta) {
+      mkdirp(ruta, function(err) {
+        if (err) {
+          console.log('Error en la ruta esta');
+        } else {
+          let path = path.join(ruta, nombreArchivo);
+          if (!fs.existsSync(path)) {
+            fs.writeFile(path, html, function(err) {
+              if (err) {
+                return -1;
+              } else {
+                console.log('Archivo guardado');
+              }
+            });
+          }
         }
-      }
-    })
+      });
+    }
+  }
+
+  /*
+   * Fijar el medio por el cual se enviaran los correos
+   * confSMTP: Es la configuracion SMTP del servidor que enviara el correo
+   */
+  fijaTransporte(confSMTP) {
+    this.transporter = nodemailer.createTransport(confSMTP);
+  }
+
+  enviarCorreo({template, locals, send, message, smtp}) {
+    this.dameTemplateHTML(template)
+      .then(data => {
+        let html = dot.template(data[0]);
+        html = html(locals);
+
+        let subject = dot.template(data[1]);
+        subject = subject(locals);
+        let inline = juice(html);
+
+        return Promise.all([inline, subject]);
+      })
+      .then(datos => {
+        mailOptions.html = datos[0];
+        mailOptions.subject = datos[1];
+
+        nodemailer.createTransport(smtp).sendMail(message, (error, info) => {
+          if (error) {
+            console.log(error);
+            event.sender.send('respuestaEnvioCorreo', false);
+            return -1;
+          } else {
+            console.log(info);
+            event.sender.send('respuestaEnvioCorreo', true);
+          }
+        });
+      });
   }
 }
 
-/*
- * Fijar el medio por el cual se enviaran los correos
- * confSMTP: Es la configuracion SMTP del servidor que enviara el correo
- */
-fijaTransporte(confSMTP){
-    this.transporter = nodemailer.createTransport(confSMTP);;
-}
-
-enviaCorreo({template,locals,send,message,smtp}){ 
-
-  dameTemplateHTML(template)
-    .then(data => {
-      let html = dot.template(data[0])
-      html = html(locals)
-
-      let subject = dot.template(data[1])
-      subject = subject(locals)
-      let inline = juice(html)
-
-      return Promise.all([inline, subject])
-    }).then(
-      datos => {
-        mailOptions.html = datos[0]
-        mailOptions.subject = datos[1]
-
-          nodemailer.createTransport(smtp).sendMail(message, (error, info) => {
-          if (error) {
-            console.log(error)
-            event.sender.send("respuestaEnvioCorreo", false)
-            return -1
-          } else {
-            console.log(info)
-            event.sender.send("respuestaEnvioCorreo", true)
-          }
-        })
-      }
-    )
-}
-
-
-}
-
+module.exports = ClienteCorreo;
