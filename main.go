11eb1f5d722300672d971429338ecc024845e02b
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-01-09 12:25:43 -0600

    Se agregan server para archivos estaticos en el root

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/dannywolfmx/ReSender/models"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	clientes := []models.Cliente{}
+
 	server := gin.Default()
+
+	//Archivos estaticos
+	server.Use(static.Serve("/", static.LocalFile("./assets", true)))
+	server.GET("/clientes", func(c *gin.Context) {
+		c.JSON(http.StatusOK, clientes)
+	})
+
+	server.POST("/clientes", func(c *gin.Context) {
+		var cliente models.Cliente
+		if err := c.ShouldBindJSON(&cliente); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		clientes = append(clientes, cliente)
+
+	})
+
 	server.Run()
 }
