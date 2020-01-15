98f05a2b18b213b2486f635ff0a863efeae6bc96
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-01-15 12:31:11 -0600

    Se agrega clista de clientes para mostrar en el HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 			}
 		}).catch((error) =>{
 			//Posible error en la conexion
-			alert("Error de conexion")
+			console.log("Error de conexion")
 		})
 
+	})
+
+	//Listar clientes en la vista
+	const clientesDIV = document.getElementById("clientes")
+	
+	fetch('clientes').then((r)=>{
+		return r.json()
+	}).then((clientes) =>{
+		//Agregar clientes a la vista html
+		clientes.map(c => {
+			let div = document.createElement("div")
+			let span = document.createElement("span")
+			let text = document.createTextNode(c.nombre)
+			span.appendChild(text)
+			div.appendChild(span)
+			clientesDIV.appendChild(div)
+		})
+	}).catch((error)=>{
+		console.log("Error al obtener clientes: ", error )
 	})
 
 })
