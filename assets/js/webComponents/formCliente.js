accc42e8b668b83d915a3ad0c470882a068abda5
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-01-22 20:16:14 -0600

    formulario funncional

--- assets/js/webComponents/formCliente.js
+++ assets/js/webComponents/formCliente.js
@@ -1,44 +1,48 @@
-class FormCliente extends HTMLElement{
+class FormCliente extends HTMLFormElement{
 	constructor(){
 		super()
-		this.attachShadow({mode:"open"})
-		this.template()	
+		this._botonEnviar = document.createElement("input")
+		this._botonEnviar.type = "submit"
+		this._botonEnviar.className = "stack icon-paper-plane"
 	}
 
-	template(){
-		this.shadowRoot.innerHTML = `
-			<link rel="stylesheet" href="https://unpkg.com/picnic">
-			<form>
-				Agregar Cliente:
-				<input type="number" name="id" placeholder="Agregar id"/>
-				<input type="text" name="nombre" placeholder="Agregar cliente"/>
-				<input type="submit" id="boton" class="stack icon-paper-plane"/>
-			</form>
+	_render(){
+			
+		this.innerHTML = `
+			Agregar Cliente:
+			<input type="number" name="id" placeholder="Agregar id"/>
+			<input type="text" name="nombre" placeholder="Agregar cliente"/>
 		`
+	}
+
+	//Enviar el formulario al servidor
+	enviarFormulario(){
+		fetch('./clientes',{
+			method:'POST',
+			body:new FormData(this)
+		}).then((respuesta) => {
+			//Limpiar el formulario si la respuesta es Ok
+			if(respuesta.ok){
+				this.reset()
+			}else{
+				//Notificar error en el formato
+				alert("Error: revisa tus datos")
+			}
+		}).catch((error) =>{
+			//Posible error en la conexion
+			console.log("Error de conexion")
+		})
 	}
 
 	connectedCallback(){
 		//Accion al enviar formulario
-		 this.shadowRoot.getElementById("boton").addEventListener("click", (e)=>{
+		 this._render()	
+		 this.appendChild(this._botonEnviar)
+		 this._botonEnviar.addEventListener("click", (e)=>{
 			e.preventDefault()
-			fetch('./clientes',{
-				method:'POST',
-				body:new FormData(this)
-			}).then((respuesta) => {
-				//Limpiar el formulario si la respuesta es Ok
-				if(respuesta.ok){
-					this.reset()
-				}else{
-					//Notificar error en el formato
-					alert("Error: revisa tus datos")
-				}
-			}).catch((error) =>{
-				//Posible error en la conexion
-				console.log("Error de conexion")
-			})
-			
+			this.enviarFormulario()	
 		})
 	}
 }
 
-customElements.define("form-cliente", FormCliente)
+customElements.define("form-cliente", FormCliente, {extends: "form"})
