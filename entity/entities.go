b133a2125b65ed19a84dbafa2cdc784572153be5
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-02-27 13:37:32 -0600

    Se agrega folder entity para manejo del modelo

---
+++ entity/entities.go
@@ -0,0 +1,31 @@
+package entity
+
+type orm struct {
+	ID        string
+	CreatedAt string
+	UpdatedAt string
+}
+
+type Order struct {
+	orm
+	Number  string
+	Clients *Client
+	Mails   []MailDirection
+	Files   []File
+}
+
+type Client struct {
+	orm
+	Name string
+}
+
+type MailDirection struct {
+	orm
+	Direction string
+}
+
+type File struct {
+	orm
+	Path  string
+	Title string
+}
