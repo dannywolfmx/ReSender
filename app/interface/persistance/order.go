6b0f4c3f91a9b373c79f34c3e42d119b7a0a7dfd
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-03-05 21:42:17 -0600

    Se programa funcionalidad para sqlite

--- app/interface/persistance/order.go
+++ app/interface/persistance/order.go
@@ -1,16 +1,19 @@
-package persistence
+package persistance
 
 import (
-	"sync"
-
 	"github.com/dannywolfmx/ReSender/app/domain/model"
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/xid"
 )
 
 type orderRepository struct {
-	mu *sync.Mutex
 	db *sqlx.DB
+}
+
+func NewOrderRepository(db *sqlx.DB) *orderRepository {
+	return &orderRepository{
+		db: db,
+	}
 }
 
 func (r *orderRepository) Save(m *model.Order) error {
@@ -27,7 +30,15 @@ func (r *orderRepository) Save(m *model.Order) error {
 }
 
 func (r *orderRepository) FindByInvoice(invoice string) (*model.Order, error) {
-	return nil, nil
+	order := new(model.Order)
+	query := `
+		SELECT * from order LIMIT 1 WHERE invoice = $1
+	`
+	err := r.db.Get(order, query, invoice)
+	if err != nil {
+		return nil, err
+	}
+	return order, nil
 }
 
 func (r *orderRepository) All() ([]*model.Order, error) {
