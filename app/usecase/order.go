174333668e0050c52831d04cc08ab0b809446d24
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-03-05 21:39:54 -0600

    Correccion de nombre de usercase -> usecase

---
+++ app/usecase/order.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"github.com/dannywolfmx/ReSender/app/domain/model"
+	"github.com/dannywolfmx/ReSender/app/domain/repository"
+	"github.com/dannywolfmx/ReSender/app/domain/service"
+)
+
+type OrderUseCase interface {
+	ListOrder() ([]*model.Order, error)
+	RegisterOrder(string, string) error
+}
+
+type orderUsecase struct {
+	repo    repository.Order
+	service *service.OrderService
+}
+
+func NewOrderUsecase(repo repository.Order, service *service.OrderService) *orderUsecase {
+	return &orderUsecase{
+		repo:    repo,
+		service: service,
+	}
+}
+
+func (o *orderUsecase) ListOrder() ([]*model.Order, error) {
+	orders, err := o.repo.All()
+	if err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
+func (o *orderUsecase) RegisterOrder(number, invoice string) error {
+	return nil
+}
