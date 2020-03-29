599e454bdd7b6b9c1b3b56834a89859fbef03693
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-03-29 17:34:43 -0600

    Creacion de sistema de notificaciones Closes #14

--- assets/component/dashboard.js
+++ assets/component/dashboard.js
@@ -1,6 +1,10 @@
 import { html } from "https://unpkg.com/lit-html?module";
 
 const dashboard = () => {
-  return html`<list-clients />`;
+  return html`<toast-noti
+    titulo="Titulo"
+    message="Prueba mensaje"
+    duration="5000"
+  />`;
 };
 export { dashboard };
---
+++ assets/component/toast.js
@@ -0,0 +1,80 @@
+import { html, render } from "https://unpkg.com/lit-html?module";
+
+class Toast extends HTMLElement {
+  constructor() {
+    super();
+    this.toast = document.createElement("div");
+    this.appendChild(this.toast);
+    this._update();
+  }
+  connectedCallback() {
+    this.toast.setAttribute("class", "toast");
+  }
+
+  attributeChangedCallback(attr, oldval, newval) {
+    if (attr === "titulo") {
+      this.titulo = newval;
+      this._update();
+    } else if (attr == "message") {
+      this.message = newval;
+      this._update();
+    } else if (attr == "duration") {
+      this._duration(newval);
+    }
+  }
+
+  static get observedAttributes() {
+    return ["titulo", "message", "duration"];
+  }
+  //Determinar cuanto turara esta notificacion
+  _duration(miliseconds) {
+    this._show();
+    console.log("Mostrando");
+    setTimeout(this._hide.bind(this), miliseconds);
+  }
+  _show() {
+    //Cambiar el opacity para mostrar el elemento
+    this.toast.setAttribute("style", "opacity:1");
+  }
+
+  _hide() {
+    this.toast.setAttribute("style", "opacity:0");
+  }
+
+  _toastHeader() {
+    return html`
+      <div class="toast-header">
+        <img src="..." class="rounded mr-2" alt="..." />
+        <strong class="mr-auto">${this.titulo}</strong>
+        <small>11 mins ago</small>
+        <button
+          type="button"
+          class="ml-2 mb-1 close"
+          data-dismiss="toast"
+          aria-label="Close"
+          @click=${this._hide}
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    `;
+  }
+
+  _toastBody() {
+    return html`
+      <div class="toast-body">
+        ${this.message}
+      </div>
+    `;
+  }
+
+  _template() {
+    return html` ${this._toastHeader()} ${this._toastBody()}`;
+  }
+
+  _update() {
+    render(this._template(), this.toast, { eventContext: this });
+  }
+}
+
+customElements.define("toast-noti", Toast);
