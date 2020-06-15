b0dfef550319f1004f23cd7a1a583a1b96d60528
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-06-15 12:48:35 -0500

    Se alimina archivos tareas en pro de usar el todo.org

--- tareas.mk
+++
@@ -1,5 +0,0 @@
-* Realizar un formulario para enviar las ordenes de compra
-* Crear un CRUD para los clientes
-* Crear un CRUD para las ordenes de compra
-* Generar una zona para subir los archivos
-
