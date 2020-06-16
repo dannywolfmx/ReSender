e37d4bd9007bb1cc984e7ecc5f10b34e4a0cc952
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-06-15 22:38:15 -0500

    Mejora en los comentarios y se agrega estado a una orden de compra

--- app/domain/model/model.go
+++ app/domain/model/model.go
@@ -23,6 +23,7 @@ Profiles:[
        Client{
           Orders: [
               Order{
+                Estado: 0,
                 Number: "ABS123",
                 Invoice: "Fac123",
                  Mails{
@@ -51,61 +52,104 @@ type Orm struct {
 
 //Order estructura que almacena la informacion de una orden de compra
 type Order struct {
-	Orm `json:"orm"`
+	Orm      `json:"orm"`
+	ClientID uint `json:"client_id"`
 
-	//Number representa al numero de la orden de compra
-	Number string `json:"number"`
+	//Estado representa el estatus en el que se encuentra esta orden
+	// 0 - No enviada {ESTADO DEFAULT}
+	// 1 - Enviada
+	// TODO - Algunos clientes no tienen la necesidad de este estado, pendiente de encontrar una mejor solución
+	// 2 - Pendiente de orden de entrada a almacen
+	Estado uint `json:"estado"`
+
+	//Files archivos relacionados con esta orden de compra
+	Files []File `json:"files"`
 
 	//Invoice representa al numero de la factura
-	Invoice  string `json:"invoice"`
-	ClientID uint   `json:"client_id"`
+	Invoice string `json:"invoice"`
 
 	//Mails lista de direcciones de correo
 	Mails []MailDirection `json:"mails" gorm:"many2many:senders"`
-	Files []File          `json:"files"`
+
+	//Number representa al numero de la orden de compra
+	Number string `json:"number"`
 }
 
 //Client es una estructura que almacena la informacion personal de este cliente asi como su relacion con las ordenes
 type Client struct {
-	Orm    `json:"orm"`
-	Name   string `json:"name"`
+	Orm `json:"orm"`
+
+	//ProfiletID Id del perfil al que pertenece este cliente
+	ProfiletID uint `json:"profilet_id"`
+
+	//Name nombre del cliente
+	Name string `json:"name"`
+
+	//Order ordenes relacionadas con este cliente
 	Orders []Order
 }
 
 //MailDirection es una estructura que almacena la informacion que un correo electrónico pueda tener
 type MailDirection struct {
-	Orm       `json:"orm"`
+	Orm `json:"orm"`
+
+	//Direction direccion de correo electronico. Ej "prueba@gmail.com"
 	Direction string `json:"direction"`
 }
 
-//File es una estructura que almacena la metadata y localizacion de un archivo
+//File es una estructura que almacena la metadata y localización de un archivo
 type File struct {
 	Orm     `json:"orm"`
-	Path    string `json:"path"`
-	Title   string `json:"title"`
-	OrderID uint   `json:"order_id"`
+	OrderID uint `json:"order_id"`
+
+	//Path direccion en la que se encuentra almacenado el archivo
+	Path string `json:"path"`
+
+	//Title titulo del archivo
+	Title string `json:"title"`
 }
 
 //Profile es una estructura que almacena la informacion de un usuario
 type Profile struct {
-	Orm                 `json:"orm"`
-	DefaultMailConfigID uint         `json:"default_mail_config_id"`
-	MailConfig          []MailServer `json:"mail_config"`
-	Name                string       `json:"name"`
-	ImageAvatarPath     string       `json:"image_avatar_path"`
+	Orm `json:"orm"`
+
+	//Clients lista de clientes que estan asociados con el perfil
+	Clients []Client `json:"clients"`
+
+	//DefailtMailConfigId la configuracion que esta marcada por defecto
+	//Nota: esto no esta relacionado con GORM
+	DefaultMailConfigID uint `json:"default_mail_config_id"`
+
+	//ImageAvatarPath Imagen del perfil
+	ImageAvatarPath string `json:"image_avatar_path"`
+
+	//MailConfig configuraciones asociadas al perfil del usuario
+	MailConfig []MailServer `json:"mail_config"`
+
+	//Name nombre del perfil
+	Name string `json:"name"`
 }
 
 //MailServer almacena la informacion de configuracion de un servidor de correos
 type MailServer struct {
 	Orm `json:"orm"`
-	//Struct fields
-	Address string `json:"address"`
-	//Alias de este perfil del servidor
-	Alias string `json:"alias"`
-	//From describe el correo electronico del remitente
-	From     string `json:"from"`
-	Password string `json:"password"`
-	Server   string `json:"server"`
+
 	//Relationship
 	ProfiletID uint `json:"profilet_id"`
+
+	//Struct fields
+	Address string `json:"address"`
+
+	//Alias de este perfil del servidor
+	Alias string `json:"alias"`
+
+	//From describe el correo electronico del remitente
+	From string `json:"from"`
+
+	//Password password del servidor de mail
+	//TODO  verificar si se realiza algun tipo de encriptacion
+	Password string `json:"password"`
+
+	//Server ???
+	Server string `json:"server"`
 }
