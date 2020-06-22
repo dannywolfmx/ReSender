941523bbf78d7f45f0cf968e10ea52d4ec3f40d0
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-06-22 14:01:10 -0500

    create profile usecase

--- app/usecase/profile.go
+++ app/usecase/profile.go
@@ -1,9 +1,13 @@
 package usecase
 
-import "github.com/dannywolfmx/ReSender/app/domain/model"
+import (
+	"github.com/dannywolfmx/ReSender/app/domain/model"
+	"github.com/dannywolfmx/ReSender/app/domain/repository"
+	"github.com/dannywolfmx/ReSender/app/domain/service"
+)
 
 //ProfileUseCase represent all the activities a 'profile' can do
-type ProfileUseCase interface{
+type ProfileUsecase interface {
 
 	//Create a new profile and return a nil error if the transactions workds.
 	Create(profile *model.Profile) error
@@ -11,10 +15,10 @@ type ProfileUseCase interface{
 	//Create password to the profile and return an error if the transaction doesnt work
 	SetPassword(password string) error
 
-	//Add a new client to the profile client list 
+	//Add a new client to the profile client list
 	//Search a profile by ID
 	//Set a relationship beetween the new client
-	AddClient(profileID uint, client *model.Client) error 
+	AddClient(profileID uint, client *model.Client) error
 
 	//Delete profile account
 	Delete(profileID uint) (*model.Profile, error)
@@ -22,3 +26,17 @@ type ProfileUseCase interface{
 	//Update a profile, return the new profile and error
 	Update(profile *mode.Profile) (*model.Profile, error)
 }
+
+type profileUsecase struct {
+	repo    repository.Profile
+	service *service.ProfileService
+}
+
+func NewProfileUsecase(repo repository.Profile, service *service.ProfileService) *profileprofileUsecase {
+	return &profileUsecase{
+		reto:    repo,
+		service: service,
+	}
+}
+
+//TODO implment ProfileUsecase de profileUsecase
