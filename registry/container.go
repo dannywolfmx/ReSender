2f45b11bce13eb57c4545948cd233cc3bfaaf98d
Daniel <daniel@localhost.localdomain>
2020-12-27 12:36:41 -0600

    Se elimina la dependencia a la libreria de inyecci
    Elimina la libreria de inyeccion de dependencias

--- registry/container.go
+++ registry/container.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/sarulabs/di"
 
+	"github.com/dannywolfmx/ReSender/app"
 	appModel "github.com/dannywolfmx/ReSender/app/domain/model"
 	appService "github.com/dannywolfmx/ReSender/app/domain/service"
 	appRepository "github.com/dannywolfmx/ReSender/app/repository/gormrepo"
@@ -53,6 +54,31 @@ func authUsecaseGORM(connDB *gorm.DB) (auth.AuthUsecase, error){
 	return authUsecase.NewAuthUsecase(repo, service), nil
 }
 
+func OrderUsecaseGORM(connDB *gorm.DB) (app.OrderUsecase, error){
+	repo := appRepository.NewOrderRepository(connDB)
+	service := appService.NewOrderService(repo)
+	return appUsecase.NewOrderUsecase(repo, service), nil
+}
+
+func ClientUsecaseGORM(connDB *gorm.DB) (app.ClientUsecase, error){
+	repo := appRepository.NewClientRepository(connDB)
+	service := appService.NewClientService(repo)
+	return appUsecase.NewClientUsecase(repo, service), nil
+}
+
+func ProfileUsecaseGORM(connDB *gorm.DB) (app.ProfileUsecase, error){
+	//Repositorio del profile
+	repository := appRepository.NewProfileRepository(connDB)
+
+	//Servicio del profile
+	//El servicio requiere hacer operaciones con el repositorio por lo que se envia uno funcional
+	//Dependiedo de la funcionalidad puede compartir el mismo repo que el usecase
+	service := appService.NewProfileService(repository)
+
+	//Cramos un usecase con un repositorio y un repositorio
+	return appUsecase.NewProfileUsecase(repository, service), nil
+}
+
 func gormSqliteConnection(dataBasePath string) (*gorm.DB, error){
 		//Fijar ruta de la db y el tipo de db
 		db, err := gorm.Open("sqlite3", dataBasePath)
@@ -64,85 +90,6 @@ func gormSqliteConnection(dataBasePath string) (*gorm.DB, error){
 		//con el motivo de que solo hacerlo cuando sea necesario
 		migrarDBGorm(db)
 		return db, nil
-}
-
-
-func NewContainer() (*Container, error) {
-	dataBaseFile := "/home/daniel/Programacion/ReSender/db/data/data.db"
-
-	//dataBaseFile := "/tmp/data.db"
-	build, err := di.NewBuilder()
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = build.Add([]di.Def{
-		{
-			Name: "gormSqlite",
-			Build: func(ctn di.Container) (interface{}, error) {
-				//Fijar ruta de la db y el tipo de db
-				db, err := gorm.Open("sqlite3", dataBaseFile)
-				if err != nil {
-					return nil, err
-				}
-				//Migrar base de datos para hacer match con estructuras
-				migrarDBGorm(db)
-				return db, err
-			},
-			Close: func(db interface{}) error {
-				return db.(*gorm.DB).Close()
-			},
-		}, {
-			Name: "order-usecase",
-			Build: func(ctn di.Container) (interface{}, error) {
-				//connDB := ctn.Get("sqlite").(*sqlx.DB)
-				connDB := ctn.Get("gormSqlite").(*gorm.DB)
-				repo := appRepository.NewOrderRepository(connDB)
-				service := appService.NewOrderService(repo)
-				return appUsecase.NewOrderUsecase(repo, service), nil
-			},
-		}, {
-			Name: "client-usecase",
-			Build: func(ctn di.Container) (interface{}, error) {
-				connDB := ctn.Get("gormSqlite").(*gorm.DB)
-				//TODO: Revisar por que no devuelvo un puntero en el repositorio
-				repo := appRepository.NewClientRepository(connDB)
-				service := appService.NewClientService(repo)
-				return appUsecase.NewClientUsecase(repo, service), nil
-			},
-		}, {
-			// CONTENEDOR PARA PROFILE
-			//Nombre del contenedor
-			Name: "profile-usecase",
-			//Forma en que se va a construir el contenedor
-			Build: func(ctn di.Container) (interface{}, error) {
-
-				//Base de datos seleccionada
-				connDB := ctn.Get("gormSqlite").(*gorm.DB)
-
-				//TODO: Revisar por que no devuelvo un puntero en el repositorio
-				//Repositorio del profile
-				repository := appRepository.NewProfileRepository(connDB)
-
-				//Servicio del profile
-				//El servicio requiere hacer operaciones con el repositorio por lo que se envia uno funcional
-				//Dependiedo de la funcionalidad puede compartir el mismo repo que el usecase
-				service := appService.NewProfileService(repository)
-
-				//Cramos un usecase con un repositorio y un repositorio
-				return appUsecase.NewProfileUsecase(repository, service), nil
-			},
-		},
-	}...)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Container{
-		ctn: build.Build(),
-	}, nil
 }
 
 //migrarDBGorm se encarga de realizar el proceso de migracion de las tablas del modelo
@@ -160,12 +107,4 @@ func migrarDBGorm(db *gorm.DB) {
 		//Auth models tables
 		&authModel.User{},
 	)
-}
-
-func (c *Container) Resolve(name string) interface{} {
-	return c.ctn.Get(name)
-}
-
-func (c *Container) Clean() error {
-	return c.ctn.Clean()
 }
